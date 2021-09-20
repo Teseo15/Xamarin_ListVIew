fafe01bbb953023521c2c903de6d998354568527
Teseo15 <brayan.blas.egaovil30@gmail.com>
2021-09-20 15:24:24 -0500

    Agregar archivos de proyecto.

---
+++ Lab6/Lab6/ListViewCode.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewCode : ContentPage
+    {
+        public ListViewCode()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(Object sender, EventArgs e)
+        {
+            listView.ItemsSource = new string[]
+            {
+                "mono",
+                "monodroide",
+                "monotouch",
+                "monorail",
+                "monodevelop",
+                "monotone",
+                "monopoly",
+                "monomodal",
+                "mononucleosis"
+            };
+
+        }
+    }
+}
---
+++ Lab6/Lab6/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            ButtonCode.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewCode());
+            };
+            ButtonDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewDemo());
+            };
+            ButtonSource.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewSource());
+            };
+
+
+
+        }
+    }
+
+}
